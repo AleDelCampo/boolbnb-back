043c5930de51fec77a0bfc10aea9a9bd19f19c4e
andrealivornese <andrealivornese@gmail.com>
2024-05-14 18:28:24 +0200

    link model apartment

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Apartment extends Model
 {
     use HasFactory;
+
+    protected $fillable=['title','slug','n_rooms','n_beds','n_bathrooms','m2','image','is_visible','description','address','latitude','longitude','user_id'];
+
+    public function sponsorships(){
+        return $this->belongsToMany(Sponsorship::class);
+    }
 }
