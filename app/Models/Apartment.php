a831431456cf5bc8e88c286c38040c6b8584b2ec
alberto <alberto.taddeucci@gmail.com>
2024-05-14 18:30:11 +0200

    add to Model Appartments: services

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -9,9 +9,15 @@ class Apartment extends Model
 {
     use HasFactory;
 
-    protected $fillable=['title','slug','n_rooms','n_beds','n_bathrooms','m2','image','is_visible','description','address','latitude','longitude','user_id'];
+    protected $fillable = ['title', 'slug', 'n_rooms', 'n_beds', 'n_bathrooms', 'm2', 'image', 'is_visible', 'description', 'address', 'latitude', 'longitude', 'user_id'];
 
-    public function sponsorships(){
+    public function sponsorships()
+    {
         return $this->belongsToMany(Sponsorship::class);
+    }
+
+    public function services()
+    {
+        return $this->belongsToMany(Service::class);
     }
 }
