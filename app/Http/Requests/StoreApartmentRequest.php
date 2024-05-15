39d99cff3202ecb640174482c5465ef3504ccb0e
AleHashirama <alehashirama@gmail.com>
2024-05-15 16:49:15 +0200

    Add: Validation

--- app/Http/Requests/StoreApartmentRequest.php
+++ app/Http/Requests/StoreApartmentRequest.php
@@ -22,7 +22,36 @@ class StoreApartmentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|max:100',
+            'description' => 'max:8000',
+            'image' => 'file|max:2048|nullable|mimes:jpg,png',
+            'n_rooms' => 'required|max:100',
+            'n_beds' => 'required|max:100',
+            'n_bathrooms' => 'required|max:100',
+            'squared_meters' => 'required|max:4000',
+            'is_visible' => 'required',
+            'address' => 'required|max:100',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'Inserisci nome Albergo',
+            'title.max' => "Puoi usare al massimo :max caratteri",
+            'description.max' => "Puoi usare al massimo :max caratteri",
+            'image.mimes' => "Inserisci un immagine",
+            'image.max' => "Peso limite 2048 KB",
+            'n_rooms.required' => 'Inserisci almeno una stanza',
+            'n_rooms.max' => ':max stanze',
+            'n_beds.required' => 'Inserisci almeno un posto letto',
+            'n_beds.max' => ':max letti',
+            'n_bathrooms.required' => 'Inserisci almeno un bagno',
+            'n_bathrooms.max' => ':max bathrooms',
+            'squared_meters.required' => 'Inserisci area Albergo',
+            'squared_meters.max' => 'Area massima di :max metri quadrati',
+            'address.required' => 'Inserisci un indirizzo valido',
+            'address.max' => 'Non credo esistano vie lunghe :max caratteri!!',
         ];
     }
 }
--- app/Http/Requests/StoreServiceRequest.php
+++ app/Http/Requests/StoreServiceRequest.php
@@ -9,20 +9,20 @@ class StoreServiceRequest extends FormRequest
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|max:100',
+            'icon' => 'required',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Inserisci un nome!!',
+            'name.max' => "Puoi usare al massimo :max caratteri",
+            'icon.required' => 'Inserisci un icona per il servizio!!',
         ];
     }
 }
