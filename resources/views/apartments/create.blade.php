d532e26b2f0f6dedc688d32c7296ebfdced5012a
pool.gutierrez <pool.gutierrezv@gmail.com>
2024-05-15 10:06:12 -0500

    fix: edit create

--- resources/views/apartments/create.blade.php
+++ resources/views/apartments/create.blade.php
@@ -34,7 +34,7 @@
 
       <div class="mb-2">
         <label for="address" class="form-label">Indirizzo: </label>
-        <input type="text" class="form-control {{--@error('address') is-invalid @enderror--}}" id="address" name="address">
+        <input type="text" class="form-control {{--@error('address') is-invalid @enderror--}}" id="address" name="address" value="{{ old('address') }}">
         {{--@error('address')
         <div class="invalid-feedback">
           {{$message}}
@@ -54,7 +54,7 @@
 
       <div class="mb-2">
         <label for="n_rooms" class="form-label">Numero stanze: </label>
-        <input type="number" class="form-control {{--@error('n_rooms') is-invalid @enderror--}}" id="n_rooms" name="n_rooms">
+        <input type="number" class="form-control {{--@error('n_rooms') is-invalid @enderror--}}" id="n_rooms" value="{{ old('n_rooms') }}" name="n_rooms">
         {{--@error('n_rooms')
         <div class="invalid-feedback">
           {{$message}}
@@ -64,7 +64,7 @@
 
       <div class="mb-2">
         <label for="n_beds" class="form-label">Numero letti: </label>
-        <input type="number" class="form-control {{--@error('n_beds') is-invalid @enderror--}}" id="n_beds" name="n_beds">
+        <input type="number" class="form-control {{--@error('n_beds') is-invalid @enderror--}}" id="n_beds" value="{{ old('n_beds') }}" name="n_beds">
         {{--@error('n_beds')
         <div class="invalid-feedback">
           {{$message}}
@@ -74,7 +74,7 @@
 
       <div class="mb-2">
         <label for="n_bathrooms" class="form-label">Numero bagni: </label>
-        <input type="number" class="form-control {{--@error('n_bathrooms') is-invalid @enderror--}}" id="n_bathrooms" name="n_bathrooms">
+        <input type="number" class="form-control {{--@error('n_bathrooms') is-invalid @enderror--}}" id="n_bathrooms" value="{{ old('n_bathrooms') }}" name="n_bathrooms">
         {{--@error('n_bathrooms')
         <div class="invalid-feedback">
           {{$message}}
@@ -84,7 +84,7 @@
 
       <div class="mb-2">
         <label for="squared_meters" class="form-label">Metri quadri: </label>
-        <input type="number" class="form-control {{--@error('squared_meters') is-invalid @enderror--}}" id="squared_meters" name="squared_meters">
+        <input type="number" class="form-control {{--@error('squared_meters') is-invalid @enderror--}}" id="squared_meters" value="{{ old('squared_meters') }}" name="squared_meters">
         {{--@error('squared_meters')
         <div class="invalid-feedback">
           {{$message}}
