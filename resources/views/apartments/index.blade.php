b11b00579f5fd0cdb853d4c80cfe795f31010af4
alberto <alberto.taddeucci@gmail.com>
2024-05-18 15:33:57 +0200

    feat: aggiunto stile index

--- resources/views/apartments/index.blade.php
+++ resources/views/apartments/index.blade.php
@@ -5,45 +5,61 @@
     <div class="container">
 <div class="d-flex justify-content-between my-4">
     <h2 class="fs-4 text-secondary">Appartamenti</h2>
-    <a class="btn btn-primary" href="{{route('admin.apartments.create')}}">Aggiungi</a>
 </div>
-    
-    </div>
+
+</div>
 </section>
 
 <section>
     <div class="container">
+        
+        <div class="row justify-content-center py-5">
+            <a class="btn my_bg_color fw-bold col-6" href="{{route('admin.apartments.create')}}">Aggiungi un nuovo appartamento</a>
+            
+        </div>
 
-        <table class="table my-4">
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Title</th>
-                <th scope="col">Descrizione</th>
-                <th scope="col"></th>
-                {{-- <th scope="col"></th> --}}
-              </tr>
-            </thead>
+        <div class="row">
+            @forelse ($apartments as $apartment)
+            <div class="col-6">
+                <div id="tile" class="row p-3 my-3">
+                    <div id="img-container" class="col-3 rounded-2">
+                        <img  src="{{asset('storage/' . $apartment->image)}}" alt="">
+        
+                    </div>
+                    <div  class="col-8" >
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="fw-bold pt-1 pb-2">
+                                    {{$apartment->title}}
+                                </div>
+                                <div>
+                                    {{$apartment->address}}
+                                </div>
+                            </div>
+                            <div class="col-6 d-flex justify-content-end align-items-center">
+                                <div>
+                                    <a class="btn my_btn" href="{{route('admin.apartments.create')}}" href="{{route('admin.apartments.show', $apartment)}}">Visualilzza</a>
+                                    
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+        
+                </div>
+    
+            </div>
+            @empty
+            nulla
+                {{-- <tr>
+                 <td>Nessun Appartamento</td>
+                </tr> --}}
+            @endforelse
 
-            <tbody>
-              <tr>
-               @forelse ($apartments as $apartment)
-                   <tr>
-                    <th scope="row">{{$apartment->id}}</th>
-                    <td>
-                        <a href="{{route('admin.apartments.show', $apartment)}}">{{$apartment->title}}</a></td>
-                    <td>{{$apartment->description}}</td>
-                    <td>
-                        <a href="{{route('admin.apartments.edit', $apartment)}}"><i class="fa-solid fa-pen-to-square"></i></a>
-                    </td>
-                   </tr>
-               @empty
-                   <tr>
-                    <td>Nessun Appartamento</td>
-                   </tr>
-               @endforelse
-            </tbody>
-          </table>
+        </div>
+                   
+
+        
     </div>
 </section>
 @endsection
