f0cb6f844caec6f1df902c00cfe0912edee7298e
pool.gutierrez <pool.gutierrezv@gmail.com>
2024-05-20 10:19:31 -0500

    fix: register style

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -42,7 +42,7 @@
 
                         {{-- e-mail --}}
                         <div class="mb-4 row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}<small>*</small></label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}<span class="obg-field">*</span></label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  autocomplete="email">
@@ -57,7 +57,7 @@
 
                         {{-- password --}}
                         <div class="mb-4 row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}<small>*</small></label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}<span class="obg-field">*</span></label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"  autocomplete="new-password">
@@ -72,7 +72,7 @@
 
                         {{-- conferma password --}}
                         <div class="mb-4 row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password') }}<small>*</small></label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password') }}<span class="obg-field">*</span></label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation"  autocomplete="new-password">
@@ -107,7 +107,7 @@
                         </div>
                     </form>
                     <div>
-                        <small>
+                        <small class="obg-field">
                             (*) Campo obbligatorio
                         </small>
                     </div>
@@ -155,8 +155,8 @@
 </script>
 
 <style>
-    small {
-        color: red;
-    }
+.obg-field {
+    color: red;
+}
 </style>
 @endsection
