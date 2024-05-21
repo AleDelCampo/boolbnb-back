80cf2a18a0e2ae44365da6ca6227bd16ac1d7409
andrealivornese <andrealivornese@gmail.com>
2024-05-21 09:36:26 +0200

    aggiunta nome dell'appartamento nel modale di eliminazione

--- resources/views/apartments/show.blade.php
+++ resources/views/apartments/show.blade.php
@@ -91,7 +91,7 @@
               <div class="modal-dialog">
                 <div class="modal-content">
                   <div class="modal-header">
-                    <h1 class="modal-title fs-5">Eliminare appartamento</h1>
+                    <h1 class="modal-title fs-5">{{$apartment->title}}</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
