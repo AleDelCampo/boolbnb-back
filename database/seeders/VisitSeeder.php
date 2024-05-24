cfa6bba91c32c683d80b80b375cd0af76d0c2ee1
AleHashirama <alehashirama@gmail.com>
2024-05-24 16:23:22 +0200

    visitseeder

--- database/seeders/VisitSeeder.php
+++ database/seeders/VisitSeeder.php
@@ -4,14 +4,32 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Visit;
+use App\Models\Apartment;
+use Faker\Factory as Faker;
 
 class VisitSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        $faker = Faker::create();
+
+        $apartments = Apartment::all();
+
+        for ($i = 1; $i < 20; $i++) {
+
+            $newVisit = new Visit();
+
+            $timestamp = $faker->dateTimeBetween('-2 years', now());
+            $ipAddress = $faker->ipv4;
+
+            $randomApartment = rand(1, 10);
+
+            $newVisit->timestamp_visit = $timestamp;
+            $newVisit->ip_address = $ipAddress;
+            $newVisit->apartment_id = $randomApartment;
+
+            $newVisit->save();
+        }
     }
 }
