3192c3ab1aa31867d389606783f36677b88b97ae
andrealivornese <andrealivornese@gmail.com>
2024-05-24 12:47:29 +0200

    fix della create e della edit sul controllo di tutti i campi obbligatori

--- resources/views/apartments/create.blade.php
+++ resources/views/apartments/create.blade.php
@@ -33,7 +33,7 @@
 
           <div class="mb-4  col-6" id="address-box">
             <label for="address" class="fw-bold form-label">Indirizzo <span class="obg-field">*</span></label>
-            <input type="text" class="required-field form-control @error('address') is-invalid @enderror" id="address"
+            <input type="text" class="form-control @error('address') is-invalid @enderror" id="address"
               name="address" value="" onkeyup="handleKeyUp()">
 
             <div class="auto-complete-box hide">
@@ -139,7 +139,7 @@
             @foreach($services as $service)
             <div class="form-check @error('services') is-invalid @enderror ">
 
-              <input type="checkbox" name="services[]" value="{{$service->id}}" class="btn-check"
+              <input type="checkbox" name="services[]" value="{{$service->id}}" class="btn-check required-check"
                 id="btn-check-{{$service->id}}" {{ in_array($service->id, old('services', [])) ? 'checked' : '' }}
               >
 
@@ -189,19 +189,29 @@ let flag = false;
 
 // Funzione per verificare lo stato di compilazione di tutti i campi obbligatori
 function checkRequiredFields() {
-    // Recupera tutti i campi obbligatori
-    const requiredFields = document.querySelectorAll('.required-field');
-    
-    // Verifica se tutti i campi obbligatori sono stati compilati
-    let allFieldsFilled = true;
-    requiredFields.forEach(field => {
-        if (field.value.trim() === '') {
-            allFieldsFilled = false;
-        }
-    });
+  // Recupera tutti i campi obbligatori
+  const requiredFields = document.querySelectorAll('.required-field');
+  
+  // Recupera tutte le checkbox 
+  const requiredChecks = document.querySelectorAll('.required-check');
+  
+  // Verifica se tutti i campi obbligatori sono stati compilati
+  let allFieldsFilled = true;
+  requiredFields.forEach(field => {
+      if (field.value.trim() === '') {
+          allFieldsFilled = false;
+      }
+  });
 
-    // Abilita o disabilita il pulsante di invio in base allo stato di compilazione dei campi
-    document.querySelector('#btn-submit').disabled = !allFieldsFilled;
+  let checkBoxChecked = false;
+  requiredChecks.forEach(check => {
+      if (check.checked == true) {
+        checkBoxChecked = true;
+      }
+  });
+
+  // Abilita o disabilita il pulsante di invio in base allo stato di compilazione dei campi
+  document.querySelector('#btn-submit').disabled = !(allFieldsFilled && checkBoxChecked && flag);
 }
 
 // Aggiorna lo stato dei campi obbligatori ogni volta che un campo viene modificato
@@ -209,129 +219,101 @@ document.querySelectorAll('.required-field').forEach(field => {
     field.addEventListener('input', checkRequiredFields);
 });
 
+
+document.querySelectorAll('.required-check').forEach(check => {
+    check.addEventListener('input', checkRequiredFields);
+});
+
 // Controllo sull'input dell'indirizzo
-if (document.getElementById('address').value.trim() != '') {
-    // Il campo non è vuoto quindi il bottone è attivo
-    document.querySelector('#btn-submit').disabled = false;
-} else {
-    // Il campo è vuoto quindi il bottone è disattivato
+if (document.getElementById('address').value.trim() == '') {
+    // Il campo è vuoto quindi il bottone è spento
     document.querySelector('#btn-submit').disabled = true;
-}
+} 
+
 
 let streets = [];
 
 // Valida l'invio del form
 function validateForm() {
-    console.log(flag)
-    // Controllo del flag
-    if (flag) {
-        // Return true
-        return flag;
-    } else {
-        // Return false
-        return flag;
-    }
+  console.log(flag)
+  // Controllo del flag
+  if (flag) {
+      // Return true
+      return flag;
+  } else {
+      // Return false
+      return flag;
+  }
 }
 
 document.querySelector('#address').addEventListener('click', function () {
-    // Variabile settata a false così da non poter inviare i dati del form
-    flag = false;
+  // Variabile settata a false così da non poter inviare i dati del form
+  flag = false;
 
-    // Il bottone viene disattivato ogni qual volta si scrivono o cancellano caratteri
-    document.querySelector('#btn-submit').disabled = true;
+  // Il bottone viene disattivato ogni qual volta si scrivono o cancellano caratteri
+  document.querySelector('#btn-submit').disabled = true;
 });
 
 function handleKeyUp(event) {
-    // La variabile viene settata a false ogni volta che vengono inseriti o cancellati caratteri
-    flag = false;
+  // La variabile viene settata a false ogni volta che vengono inseriti o cancellati caratteri
+  flag = false;
 
-    // Lista delle vie suggerite
-    const UlEle = document.getElementById('suggested-roads-list');
-    UlEle.innerHTML = '';
+  // Lista delle vie suggerite
+  const UlEle = document.getElementById('suggested-roads-list');
+  UlEle.innerHTML = '';
 
-    // Valore del campo dell'indirizzo
-    const input = document.getElementById('address').value;
+  // Valore del campo dell'indirizzo
+  const input = document.getElementById('address').value;
 
-    // Controllo sull'input che non sia vuoto
-    if (input.trim() != '') {
-        // Il bottone viene disattivato ogni qual volta si scrivono o cancellano caratteri
-        document.querySelector('#btn-submit').disabled = true;
+  // Controllo sull'input che non sia vuoto
+  if (input.trim() != '') {
+      // Il bottone viene disattivato ogni qual volta si scrivono o cancellano caratteri
+      document.querySelector('#btn-submit').disabled = true;
 
-        axios.get('http://127.0.0.1:8000/api/autocomplete-address?query=' + input)
-            .then(response => {
-                // Inserita l'array dei risultati in un array locale
-                streets = response.data.result.results;
-                console.log(streets);
+      axios.get('http://127.0.0.1:8000/api/autocomplete-address?query=' + input)
+          .then(response => {
+              // Inserita l'array dei risultati in un array locale
+              streets = response.data.result.results;
+              console.log(streets);
+          })
+          .catch(error => {
+              console.error(error);
+          });
+  } else {
+      document.querySelector('.auto-complete-box').classList.add('hide');
 
-                // Verifica se tutti i campi obbligatori sono stati compilati
-                const allFieldsFilled = checkAllFieldsFilled();
+      // controlla che tutti i campi siano stati compilati o meno e disattiva e attiva il pulsate in relazione alla scansione
+      checkRequiredFields()
+  }
 
-                // Abilita o disabilita il pulsante di invio in base allo stato di compilazione dei campi
-                document.querySelector('#btn-submit').disabled = !allFieldsFilled;
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    } else {
-        document.querySelector('.auto-complete-box').classList.add('hide');
+  if (streets.length != 0) {
+      for (let i = 0; i < streets.length; i++) {
+          const liEl = document.createElement('li');
+          const divEl = document.createElement('div');
+          divEl.innerText = streets[i].address.freeformAddress + ', ' + streets[i].address.country;
+          liEl.append(divEl);
+          liEl.classList.add('list-group-item', 'list-group-item-action');
+          UlEle.append(liEl);
 
-        // Verifica se tutti i campi obbligatori sono stati compilati
-        const allFieldsFilled = checkAllFieldsFilled();
+          liEl.addEventListener('click', function () {
+              flag = true;
+              // Viene inserito la via scelta nella casella dell'indirizzo
+              document.getElementById('address').value = liEl.innerText;
 
-        // Abilita o disabilita il pulsante di invio in base allo stato di compilazione dei campi
-        document.querySelector('#btn-submit').disabled = !allFieldsFilled;
-    }
-
-    if (streets.length != 0) {
-        for (let i = 0; i < streets.length; i++) {
-            const liEl = document.createElement('li');
-            const divEl = document.createElement('div');
-            divEl.innerText = streets[i].address.freeformAddress + ', ' + streets[i].address.country;
-            liEl.append(divEl);
-            liEl.classList.add('list-group-item', 'list-group-item-action');
-            UlEle.append(liEl);
-
-            liEl.addEventListener('click', function () {
-                flag = true;
-                // Viene inserito la via scelta nella casella dell'indirizzo
-                document.getElementById('address').value = liEl.innerText;
-
-                // Il menu viene nascosto
-                document.querySelector('.auto-complete-box').classList.add('hide');
-
-                // Verifica se tutti i campi obbligatori sono stati compilati
-                const allFieldsFilled = checkAllFieldsFilled();
-
-                // Abilita o disabilita il pulsante di invio in base allo stato di compilazione dei campi
-                document.querySelector('#btn-submit').disabled = !allFieldsFilled;
-            });
-        }
-        document.querySelector('.auto-complete-box').classList.remove('hide');
-    } else {
-        document.querySelector('.auto-complete-box').classList.add('hide');
-
-        // Verifica se tutti i campi obbligatori sono stati compilati
-        const allFieldsFilled = checkAllFieldsFilled();
-
-        // Abilita o disabilita il pulsante di invio in base allo stato di compilazione dei campi
-        document.querySelector('#btn-submit').disabled = !allFieldsFilled;
-    }
-}
-
-// Funzione per verificare se tutti i campi obbligatori sono stati compilati
-function checkAllFieldsFilled() {
-    // Recupera tutti i campi obbligatori
-    const requiredFields = document.querySelectorAll('.required-field');
-    
-    // Verifica se tutti i campi obbligatori sono stati compilati
-    let allFieldsFilled = true;
-    requiredFields.forEach(field => {
-        if (field.value.trim() === '') {
-            allFieldsFilled = false;
-        }
-    });
-
-    return allFieldsFilled;
+              // Il menu viene nascosto
+              document.querySelector('.auto-complete-box').classList.add('hide');
+              
+              // controlla che tutti i campi siano stati compilati o meno e disattiva e attiva il pulsate in relazione alla scansione
+              checkRequiredFields()
+          });
+      }
+      document.querySelector('.auto-complete-box').classList.remove('hide');
+  } else {
+      document.querySelector('.auto-complete-box').classList.add('hide');
+      
+      // controlla che tutti i campi siano stati compilati o meno e disattiva e attiva il pulsate in relazione alla scansione
+      checkRequiredFields()
+  }
 }
 </script>
 @endsection
--- resources/views/apartments/edit.blade.php
+++ resources/views/apartments/edit.blade.php
@@ -39,7 +39,7 @@
                     <div class="mb-4 col-6" id="address-box">
                         <label for="address" class="form-label fw-bold">Indirizzo <span class="obg-field">*</span>
                         </label>
-                        <input type="text" class="required-field form-control @error('address') is-invalid @enderror" id="address"
+                        <input type="text" class="form-control @error('address') is-invalid @enderror" id="address"
                             value="{{ old('address') ?? $apartment->address }}" onkeyup="handleKeyUp()" name="address">
 
                         <div class="auto-complete-box hide">
@@ -158,7 +158,7 @@
                         @foreach($services as $service)
                         <div class="form-check @error('services') is-invalid @enderror">
 
-                            <input type="checkbox" name="services[]" value="{{$service->id}}" class="btn-check"
+                            <input type="checkbox" name="services[]" value="{{$service->id}}" class="btn-check required-check"
                                 id="btn-check-{{$service->id}}" @if ($errors->any())
                             {{ in_array($service->id, old('services', [])) ? 'checked' : '' }}
                             @else
@@ -199,153 +199,137 @@
 
 <script>
     // Variabile flag, in base al suo valore (true o false) permette l'invio o meno dei dati del form
-  let flag = true;
-  
-  // Funzione per verificare lo stato di compilazione di tutti i campi obbligatori
-  function checkRequiredFields() {
-      // Recupera tutti i campi obbligatori
-      const requiredFields = document.querySelectorAll('.required-field');
-      
-      // Verifica se tutti i campi obbligatori sono stati compilati
-      let allFieldsFilled = true;
-      requiredFields.forEach(field => {
-          if (field.value.trim() === '') {
-              allFieldsFilled = false;
-          }
-      });
+    let flag = true;
+    
+    // Funzione per verificare lo stato di compilazione di tutti i campi obbligatori
+    function checkRequiredFields() {
+        // Recupera tutti i campi obbligatori
+        const requiredFields = document.querySelectorAll('.required-field');
+
+        // Recupera tutte le checkbox 
+        const requiredChecks = document.querySelectorAll('.required-check')
+        
+        // Verifica se tutti i campi obbligatori sono stati compilati
+        let allFieldsFilled = true;
+        requiredFields.forEach(field => {
+            if (field.value.trim() === '') {
+                allFieldsFilled = false;
+            }
+        });
+
+
+        let checkBoxChecked = false;
+        requiredChecks.forEach(check => {
+            if (check.checked == true) {
+            checkBoxChecked = true;
+            }
+        });
   
       // Abilita o disabilita il pulsante di invio in base allo stato di compilazione dei campi
-      document.querySelector('#btn-submit').disabled = !allFieldsFilled;
-  }
+      document.querySelector('#btn-submit').disabled = !(allFieldsFilled && checkBoxChecked && flag);
+    }
+
+    // Aggiorna lo stato dei campi obbligatori ogni volta che un campo viene modificato
+    document.querySelectorAll('.required-field').forEach(field => {
+        field.addEventListener('input', checkRequiredFields);
+    });
+
+
+    document.querySelectorAll('.required-check').forEach(check => {
+        check.addEventListener('input', checkRequiredFields);
+    });
+    
+    // Controllo sull'input dell'indirizzo
+    if (document.getElementById('address').value.trim() == '') {
+        // Il campo è vuoto quindi il bottone è spento
+        document.querySelector('#btn-submit').disabled = true;
+    } 
   
-  // Aggiorna lo stato dei campi obbligatori ogni volta che un campo viene modificato
-  document.querySelectorAll('.required-field').forEach(field => {
-      field.addEventListener('input', checkRequiredFields);
-  });
+    let streets = [];
+    
+    // Valida l'invio del form
+    function validateForm() {
+        console.log(flag)
+        // Controllo del flag
+        if (flag) {
+            // Return true
+            return flag;
+        } else {
+            // Return false
+            return flag;
+        }
+    }
   
-  // Controllo sull'input dell'indirizzo
-  if (document.getElementById('address').value.trim() != '') {
-      // Il campo non è vuoto quindi il bottone è attivo
-      document.querySelector('#btn-submit').disabled = false;
-  } else {
-      // Il campo è vuoto quindi il bottone è disattivato
-      document.querySelector('#btn-submit').disabled = true;
-  }
+    document.querySelector('#address').addEventListener('click', function () {
+        // Variabile settata a false così da non poter inviare i dati del form
+        flag = false;
+    
+        // Il bottone viene disattivato ogni qual volta si scrivono o cancellano caratteri
+        document.querySelector('#btn-submit').disabled = true;
+    });
   
-  let streets = [];
+    function handleKeyUp(event) {
+        // La variabile viene settata a false ogni volta che vengono inseriti o cancellati caratteri
+        flag = false;
+    
+        // Lista delle vie suggerite
+        const UlEle = document.getElementById('suggested-roads-list');
+        UlEle.innerHTML = '';
+    
+        // Valore del campo dell'indirizzo
+        const input = document.getElementById('address').value;
+    
+        // Controllo sull'input che non sia vuoto
+        if (input.trim() != '') {
+            // Il bottone viene disattivato ogni qual volta si scrivono o cancellano caratteri
+            document.querySelector('#btn-submit').disabled = true;
+    
+            axios.get('http://127.0.0.1:8000/api/autocomplete-address?query=' + input)
+                .then(response => {
+                    // Inserita l'array dei risultati in un array locale
+                    streets = response.data.result.results;
+                    console.log(streets);
+    
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        } else {
+            document.querySelector('.auto-complete-box').classList.add('hide');
+            
+            // controlla che tutti i campi siano stati compilati o meno e disattiva e attiva il pulsate in relazione alla scansione
+            checkRequiredFields();
+        }
+    
+        if (streets.length != 0) {
+            for (let i = 0; i < streets.length; i++) {
+                const liEl = document.createElement('li');
+                const divEl = document.createElement('div');
+                divEl.innerText = streets[i].address.freeformAddress + ', ' + streets[i].address.country;
+                liEl.append(divEl);
+                liEl.classList.add('list-group-item', 'list-group-item-action');
+                UlEle.append(liEl);
+    
+                liEl.addEventListener('click', function () {
+                    flag = true;
+                    // Viene inserito la via scelta nella casella dell'indirizzo
+                    document.getElementById('address').value = liEl.innerText;
+    
+                    // Il menu viene nascosto
+                    document.querySelector('.auto-complete-box').classList.add('hide');
+                    
+                    // controlla che tutti i campi siano stati compilati o meno e disattiva e attiva il pulsate in relazione alla scansione
+                    checkRequiredFields();
+                });
+            }
+            document.querySelector('.auto-complete-box').classList.remove('hide');
+        } else {
+            document.querySelector('.auto-complete-box').classList.add('hide');
+            
+            // controlla che tutti i campi siano stati compilati o meno e disattiva e attiva il pulsate in relazione alla scansione
+            checkRequiredFields();
+        }
+    }
   
-  // Valida l'invio del form
-  function validateForm() {
-      console.log(flag)
-      // Controllo del flag
-      if (flag) {
-          // Return true
-          return flag;
-      } else {
-          // Return false
-          return flag;
-      }
-  }
-  
-  document.querySelector('#address').addEventListener('click', function () {
-      // Variabile settata a false così da non poter inviare i dati del form
-      flag = false;
-  
-      // Il bottone viene disattivato ogni qual volta si scrivono o cancellano caratteri
-      document.querySelector('#btn-submit').disabled = true;
-  });
-  
-  function handleKeyUp(event) {
-      // La variabile viene settata a false ogni volta che vengono inseriti o cancellati caratteri
-      flag = false;
-  
-      // Lista delle vie suggerite
-      const UlEle = document.getElementById('suggested-roads-list');
-      UlEle.innerHTML = '';
-  
-      // Valore del campo dell'indirizzo
-      const input = document.getElementById('address').value;
-  
-      // Controllo sull'input che non sia vuoto
-      if (input.trim() != '') {
-          // Il bottone viene disattivato ogni qual volta si scrivono o cancellano caratteri
-          document.querySelector('#btn-submit').disabled = true;
-  
-          axios.get('http://127.0.0.1:8000/api/autocomplete-address?query=' + input)
-              .then(response => {
-                  // Inserita l'array dei risultati in un array locale
-                  streets = response.data.result.results;
-                  console.log(streets);
-  
-                  // Verifica se tutti i campi obbligatori sono stati compilati
-                  const allFieldsFilled = checkAllFieldsFilled();
-  
-                  // Abilita o disabilita il pulsante di invio in base allo stato di compilazione dei campi
-                  document.querySelector('#btn-submit').disabled = !allFieldsFilled;
-              })
-              .catch(error => {
-                  console.error(error);
-              });
-      } else {
-          document.querySelector('.auto-complete-box').classList.add('hide');
-  
-          // Verifica se tutti i campi obbligatori sono stati compilati
-          const allFieldsFilled = checkAllFieldsFilled();
-  
-          // Abilita o disabilita il pulsante di invio in base allo stato di compilazione dei campi
-          document.querySelector('#btn-submit').disabled = !allFieldsFilled;
-      }
-  
-      if (streets.length != 0) {
-          for (let i = 0; i < streets.length; i++) {
-              const liEl = document.createElement('li');
-              const divEl = document.createElement('div');
-              divEl.innerText = streets[i].address.freeformAddress + ', ' + streets[i].address.country;
-              liEl.append(divEl);
-              liEl.classList.add('list-group-item', 'list-group-item-action');
-              UlEle.append(liEl);
-  
-              liEl.addEventListener('click', function () {
-                  flag = true;
-                  // Viene inserito la via scelta nella casella dell'indirizzo
-                  document.getElementById('address').value = liEl.innerText;
-  
-                  // Il menu viene nascosto
-                  document.querySelector('.auto-complete-box').classList.add('hide');
-  
-                  // Verifica se tutti i campi obbligatori sono stati compilati
-                  const allFieldsFilled = checkAllFieldsFilled();
-  
-                  // Abilita o disabilita il pulsante di invio in base allo stato di compilazione dei campi
-                  document.querySelector('#btn-submit').disabled = !allFieldsFilled;
-              });
-          }
-          document.querySelector('.auto-complete-box').classList.remove('hide');
-      } else {
-          document.querySelector('.auto-complete-box').classList.add('hide');
-  
-          // Verifica se tutti i campi obbligatori sono stati compilati
-          const allFieldsFilled = checkAllFieldsFilled();
-  
-          // Abilita o disabilita il pulsante di invio in base allo stato di compilazione dei campi
-          document.querySelector('#btn-submit').disabled = !allFieldsFilled;
-      }
-  }
-  
-  // Funzione per verificare se tutti i campi obbligatori sono stati compilati
-  function checkAllFieldsFilled() {
-      // Recupera tutti i campi obbligatori
-      const requiredFields = document.querySelectorAll('.required-field');
-      
-      // Verifica se tutti i campi obbligatori sono stati compilati
-      let allFieldsFilled = true;
-      requiredFields.forEach(field => {
-          if (field.value.trim() === '') {
-              allFieldsFilled = false;
-          }
-      });
-  
-      return allFieldsFilled;
-  }
 </script>
 @endsection
