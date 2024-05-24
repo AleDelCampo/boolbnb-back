bc56168cf66c2b282d4046fc05d99d99ed21f961
alberto <alberto.taddeucci@gmail.com>
2024-05-24 12:12:48 +0200

    aggiunta icone seederservice

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -16,10 +16,38 @@ class ServiceSeeder extends Seeder
     {
         $services = ['wi-fi', 'piscina', 'portineria', 'posto auto', 'sauna', 'vista mare'];
 
+        $services = [
+            [
+                "name" => "Wi-fi",
+                "icon" => "<i class='fa-solid fa-wifi'></i>"
+            ],
+            [
+                "name" => "Piscina",
+                "icon" => "<i class='fa-solid fa-water-ladder'></i>"
+            ],
+            [
+                "name" => "Portineria",
+                "icon" => "<i class='fa-solid fa-door-closed'></i>"
+            ],
+            [
+                "name" => "Posto auto",
+                "icon" => "<i class='fa-solid fa-car'></i>"
+            ],
+            [
+                "name" => "Sauna",
+                "icon" => "<i class='fa-solid fa-person-booth'></i>"
+            ],
+            [
+                "name" => "Vista mare",
+                "icon" => "<i class='fa-solid fa-water'></i>"
+            ],
+        ];
+
         foreach ($services as $service) {
             $newService = new Service();
 
-            $newService->name = $service;
+            $newService->name = $service['name'];
+            $newService->icon = $service['icon'];
 
             $newService->save();
         }
