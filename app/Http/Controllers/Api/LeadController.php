777c50f8a4972fe0e55cf2a3194a7ea4cadd0993
AleHashirama <alehashirama@gmail.com>
2024-05-29 17:44:46 +0200

    Add: Messages Chart in Apartment show.

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Mail\NewContact;
 use App\Models\Lead;
+use App\Models\Apartment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
 
         // validazioni
         $validator = Validator::make($request->all(), [
@@ -34,7 +36,7 @@ class LeadController extends Controller
 
 
         // validazione non di successo
-        if($validator->fails()) {
+        if ($validator->fails()) {
             return response()->json([
                 'success' => false,
                 'errors' => $validator->errors()
@@ -59,6 +61,45 @@ class LeadController extends Controller
             'success' => true,
             'request' => $request->all(),
         ]);
+    }
 
+    public function getMessagesByApartmentIdAndYear($apartmentId, $year)
+    {
+        // Verifica se l'appartamento esiste nel database
+        $apartment = Apartment::find($apartmentId);
+        if (!$apartment) {
+            return response()->json([
+                'success' => false,
+                'error' => 'L\'appartamento non esiste.',
+            ], 404);
+        }
+
+        // Ottieni i dati dei messaggi per l'appartamento e l'anno specificati
+        $messages = Lead::where('apartment_id', $apartmentId)
+            ->whereYear('created_at', $year)
+            ->get();
+
+        // Raggruppa i messaggi per mese
+        $messagesByMonth = $messages->groupBy(function ($message) {
+            return $message->created_at->format('m'); // Restituisce solo il mese (es. '01' per gennaio)
+        });
+
+        // Conta il numero di messaggi per ogni mese
+        $messageCounts = $messagesByMonth->map(function ($messages) {
+            return $messages->count();
+        });
+
+        // Assicura che ogni mese abbia un valore, anche se 0
+        $result = [];
+        for ($i = 1; $i <= 12; $i++) {
+            $month = substr('0' . $i, -2); // Assicura che il mese sia in formato 'MM'
+            $result[$month] = $messageCounts->get($month, 0);
+        }
+
+        // Restituisci i dati dei messaggi
+        return response()->json([
+            'success' => true,
+            'result' => $result,
+        ]);
     }
 }
