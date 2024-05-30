8b69091e486941b1847fc73503510e20b3edcbd5
pool.gutierrez <pool.gutierrezv@gmail.com>
2024-05-30 08:34:27 -0500

    fix: style message

--- resources/views/leads/index.blade.php
+++ resources/views/leads/index.blade.php
@@ -5,34 +5,38 @@
 <section id="main">
     <div class="container py-2">
 
-        <a href="{{route('admin.apartments.show', $slug)}}" class="btn btn-success">Torna</a>
+        <a href="{{route('admin.apartments.show', $slug)}}" class="btn my_bg_color mt-1">Torna</a>
 
-        <h1 class="pt-2">Messaggi</h1>
+        <h1 class="p-2">Messaggi</h1>
         
         <div class="container">
-            <div class="row p-0">
+            <div class="row p-5 justify-content-center form-container rounded rounded-3">
                 @forelse ($leads as $lead)
-                <div id="tile" class="col-11 m-3 border border-dark">
-                    <div class="row p-3 my-3">
-                        <div class="row">
+                <div id="" class="mb-3 border rounded rounded-2 bg-light">
+                    <div class="row p-1 m-1">
+                        <div class="row p-2">
                             <div class="col-3">
-                                <div class="fw-bold pt-1 pb-2">
-                                    {{$lead->name}} {{$lead->surname}}
+                                <div class="pt-1 pb-2">
+                                    <div>   
+                                        <strong>
+                                            {{$lead->name}} {{$lead->surname}}
+                                        </strong>
+                                    </div>
+                                    <div>
+                                        <small>
+                                            Email: {{$lead->mail_address}}
+                                        </small>
+                                    </div>
                                 </div>
                                 <div>
-                                    <small>
-                                        Email: {{$lead->mail_address}}
-                                    </small>
-                                </div>
-                                <div>
-                                    <small>
-                                        Ricevuto: {{$lead->created_at}}
-                                    </small>
+                                    <small>Ricevuto: {{$lead->created_at}}</small>
                                 </div>
                             </div>
 
-                            <div class="col-8">
-                                <h6>Messaggio:</h6>
+                            <div class="col-9">
+                                <h6>
+                                    <strong>Messaggio:</strong>
+                                </h6>
                                 <div>
                                     {{$lead->message}}
                                 </div>
